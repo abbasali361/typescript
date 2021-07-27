3301dc9816051645796427beeaf7f77a2d39a4cd
abbasali361 <alabbashossain@gmail.com>
2021-07-27 15:35:23 +0600

    first coment

--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -1,61 +1,60 @@
-import { notEqual } from 'assert';
 import React, { useState } from 'react';
 import Contact from './Contact';
 
 interface IContact{
-    name: string,
-    email: string
+    name: string;
+    email: string;
 }
-
 const Contacts = () => {
-    const [contact, setContact] = useState<IContact>({} as IContact);
+    const [contact, setContact] = useState<IContact>({
+        name: "",
+        email: ""
+    });
     const [contactList, setContactList] = useState<IContact[]>([])
 
-  
-
-    const onClick = () =>{
-        setContactList([ ...contactList, contact])
+    const onClick = () => {
+        setContactList([...contactList, contact])
         setContact({
             name: "",
             email: ""
         })
     }
-    const onChange=(e: React.ChangeEvent<HTMLInputElement>)=> {
-        setContact({ ...contact, [e.target.name]: e.target.value})
-    }
-const handleRemove = (email: string) => {
-    const newContactList = contactList.filter(c => c.email !== email)
-    setContactList(newContactList)
-}
 
-    console.log("contactList", contactList);
+    const onChange = (e: React.ChangeEvent<HTMLInputElement>) =>  {
+        setContact({ ...contact, [e.target.name]:  e.target.value})
+    }
+    
+    const handleRemove = (email: string) => {
+        const newContactList = contactList.filter(c => c.email !== email)
+        setContactList(newContactList)
+    }
+    
     return (
         <div>
-            <h1>contact list</h1>
+            <h1>🦸 Contact list</h1>
             <div className="form">
                 <input 
                     value={contact.name}
                     onChange={onChange}
+                    name="name"
+                    placeholder="Contact Name"
                     type="text" 
-                    name="name" 
-                    placeholder="Contact Name" 
                 />
-                <input 
+                 <input 
                     value={contact.email}
                     onChange={onChange}
+                    name="email"
+                    placeholder="Contact Email"
                     type="email" 
-                    name="email" 
-                    placeholder="Contact Email" 
                 />
                 <button onClick={onClick}>Add</button>
             </div>
             {
-                contactList.map((con) =><Contact key={con.name} name={con.name} handleRemove={handleRemove} />)
+                contactList.map((con) => <Contact key={con.name} name={con.name} email={con.email} handleRemove={handleRemove}  />)
             }
             
-            {/* <Contact name="Abbas Ali" email="alabbashossain@gmail.com" /> */}
+         
         </div>
     );
 };
-
 export default Contacts;
